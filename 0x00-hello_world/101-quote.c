df3356be10c8292d52c36d6c198f72d3c02c6251
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-05 01:07:21 -0700

    Simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ *main - Prints to the Stderr
+ *
+ *Return: 1
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quote, 59);
+
+	return (1);
+}
