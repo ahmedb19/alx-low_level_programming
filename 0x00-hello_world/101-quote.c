de13ee6b6bf94fdca4468982a7e5e056b5834813
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-05 01:21:07 -0700

    Simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@ int main(void)
 	char quote[] = "and that piece of art is useful\"";
 	char quo[] = " - Dora Korpar, 2015-10-19\n";
 
-	write(1, quote, 31);
-	write(1, quo, 29);
+	write(1, quote, 32);
+	write(1, quo, 27);
 
 	return (1);
 }
