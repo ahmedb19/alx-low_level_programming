031f6c74554916e41f4e44f9b221ff90792690c3
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-05 01:19:49 -0700

    Simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,11 @@
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quote[] = "and that piece of art is useful\"";
+	char quo[] = " - Dora Korpar, 2015-10-19\n";
 
-	write(1, quote, 59);
+	write(1, quote, 31);
+	write(1, quo, 29);
 
 	return (1);
 }
