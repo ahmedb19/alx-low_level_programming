ad67b411861f54480eca0d196331972432e92c2e
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-06 08:05:52 -0700

    Success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,15 @@ int main(void)
 {
 	int i = '0';
 
-	while (i < '9')
+	while (i <= '9')
 	{
-		putchar(i);
-		putchar(',');
-		putchar(' ');
-		i++;
+		if (i != 9)
+		{
+			putchar(i);
+			putchar(',');
+			putchar(' ');
+			i++;
+		}
 	}
 	putchar('\n');
 
