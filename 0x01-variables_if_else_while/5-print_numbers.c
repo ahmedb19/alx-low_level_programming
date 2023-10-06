29eac18a57da1289dae98e8e64b88486324b232e
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-06 07:40:22 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - prints all single digit numbers of base 10 starting from 0
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char a = '48';
+
+	while (a <= '57')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+}
