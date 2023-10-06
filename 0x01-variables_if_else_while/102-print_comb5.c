662718a0b9ad7b6b115b794e0a5a185a49f80e42
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-06 08:37:19 -0700

    Software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ *main - prints all possible combinations of two two-digit numbers.
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i = 0, j;
+
+	while (i <= 99)
+	{
+		j = i;
+		while (j <= 99)
+		{
+			if (j != i)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
