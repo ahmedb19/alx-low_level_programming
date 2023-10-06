7aec6f80d9e37cf049c0958f5078e3647f40446c
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-06 07:01:08 -0700

    Pay Off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - prints the alphabet in lowercase, followed by a new line.
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int a, z;
+
+	for (a = 'a', z = 'z'; a <= z && a != 'e' & a != 'q'; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
