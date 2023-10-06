3875ffad175eb53c8800bacee18cf1af9b7a978f
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-06 07:53:50 -0700

    Smile inthe mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - prints the lowercase alphabet in reverse
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = 'z'; z >= 'a'; z--)
+		putchar('z');
+	putchar('\n');
+
+	return (0);
+}
