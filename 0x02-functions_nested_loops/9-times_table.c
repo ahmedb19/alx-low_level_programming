f38d40be2babd7eb54555bd723ae4c6a910cde2b
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-10 16:45:08 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,16 +6,21 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, n;
 
 	for (i = 0; i <= 9; i++)
 	{
+		_putchar(48);
 		for (j = 0; j <= 9; j++)
 		{
-			{
-				_putchar(',');
+			_putchar(',');
+			_putchar(' ');
+			n = i * j;
+			if (n <= 9)
 				_putchar(' ');
-			}
+			else
+				_putchar((n / 10) + 48);
+			_putchar((n % 10) + 48);
 		}
 		_putchar('\n');
 	}
