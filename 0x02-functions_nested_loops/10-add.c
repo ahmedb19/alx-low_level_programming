8b38fac88fe4f9eba6e3c71e4f6c4742a3a1655b
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-10 12:26:13 -0700

    A + B

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *add - Adds two integers and returns the result
+ *@a: 1st Integer
+ *@b: 2nd Integer
+ *
+ *Return: result
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
