2524c0139e17b065329a593bf696db6ea545e9d2
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-10 06:32:59 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *jack_bauer - prints every minute of the day of Jack Bauer,
+ *starting from 00:00 to 23:59.
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar(i);
+			_putchar(i + '0');
+			_putchar(':');
+			_putchar(j);
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
