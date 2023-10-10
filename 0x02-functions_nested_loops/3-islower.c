c42925bdcf401ee4adcbe9fcf7a6521414302ee4
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-10 03:57:31 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_islower - checks for lowercase character
+ *@c: Character to be checked
+ *
+ *Return: 1 if c is lowercase, Otherwise 0
+ */
+
+int _islower(char c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
