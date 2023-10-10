cddc0662ed6b6178f42c736ac9f4ea68eefaf306
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-10 04:19:32 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
  *print_sign - prints the sign of a number
+ *@n: Int to be checked
  *
  *Return: 1 if n is greater than 0, 0 if n is zero, -1 if n less than zero
  */
@@ -14,7 +15,7 @@ int print_sign(int n)
 	} else if (n == 0)
 	{
 		_putchar('0');
-		return(0);
+		return (0);
 	} else if (n < 0)
 	{
 		_putchar('-');
