591eb3aec7083d03381e882761db8a3f04b64a14
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-10 05:06:30 -0700

    3107cdn

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_last_digit - Prints the last digit of a number
+ *@n: Int to be checked
+ *
+ * Return: the value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int s;
+
+	s = n % 10;
+	_putchar(s);
+
+	return (s);
+}
