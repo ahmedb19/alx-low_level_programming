05ff3119923d27a72b6abaf6ffec1095238241bb
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-10 04:41:08 -0700

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - computes the absolute value of an integer
+ *@n: Integer
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n *= -1;
+       return (0);	
+}
