9717e124a9962db5dcf655772d67f64147a7a44b
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-11 11:36:30 -0700

    Naturals

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - computes and prints the sum of all the multiples of 3 or 5 below 1024
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5) == 0)
+			j += i;
+	}
+	printf("%d\n", j);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ *main -  prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ *
+ */
