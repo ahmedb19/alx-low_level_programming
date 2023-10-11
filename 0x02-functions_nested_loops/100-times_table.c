00d105cc29a46b61c0449046d6d90ba09fdac45c
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-11 11:15:46 -0700

    Balances Itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,18 +7,27 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-	if (n > 15 || n < 0)
-		_putchar('\n');
-	else
+	if (n <= 15 || n >= 0)
 	{
-		for (i = 0; i < n; n++)
+		for (i = 0; i <= n; ++i)
 		{
-			for (j = i; j < n; j++)
+			_putchar(48);
+			for (j = 1; j <= n; ++j)
 			{
-				k = j * i;
-				_putchar(k);
 				_putchar(',');
 				_putchar(' ');
+				k = i * j;
+				if (k <= 9)
+					_putchar(' ');
+				if (k <= 99)
+					_putchar(' ');
+				if (k >= 100)
+				{
+					_putchar((k / 100) + 48);
+					_putchar((k / 100) % 10 + 48);
+				} else if (k <= 99 && k >= 10)
+					_putchar((k / 10) + 48);
+				_putchar((k % 10) + 48);
 			}
 			_putchar('\n');
 		}
