3d052682dd30a7bf205f25038020453cf06e26ea
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-12 11:18:03 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - prints the numbers from 1 to 100.
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else if ((a % 3 == 0) && (a % 5 == 0))
+			printf("Fizz Buzz");
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+
+	return (0);
+}
