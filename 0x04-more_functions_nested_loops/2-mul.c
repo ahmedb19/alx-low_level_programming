407e1fa594bb0a8686fddb5d52b4c8b52e3f4225
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-12 05:24:26 -0700

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *mul - multiplies two integers
+ *@a: 1st Integer
+ *@b: 2nd Integer
+ *
+ *Return: 0
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
