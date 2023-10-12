8f581549d7fa4c91fc4c02ca3880567ae08c70ce
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-12 12:01:53 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,11 +13,11 @@ void print_triangle(int size)
 		_putchar('\n');
 	else
 	{
-		for (i = 0; i <= size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = i; j <= size; j++)
+			for (j = 1; j <= size; j++)
 			{
-				if (i + j <= size)
+				if ((i + j) <= size)
 					_putchar(' ');
 				else
 					_putchar('#');
