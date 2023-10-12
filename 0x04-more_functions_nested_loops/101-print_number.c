719a9f6876c24cdbb8c054d2078fc03b1b47ebcf
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-12 13:01:46 -0700

    Have Life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_number - prints an integer
+ *@n: Integer to be printed
+ *
+ */
+void print_number(int n)
+{
+	unsigned int s = n;
+
+	if (s < 0)
+	{
+		_putchar('-');
+		s = -s;
+	}
+	if ((s / 10) > 0)
+		print_number(s / 10);
+	_putchar((s % 10) + 48);
+}
