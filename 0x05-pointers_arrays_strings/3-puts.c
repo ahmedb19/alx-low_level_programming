c1865ff0d0b53f148321e54cfd99be48025aa37c
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-16 21:10:03 +0100

    Lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_puts - Prints a string, to stdout.
+ *@str: prints a string.
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *str != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
