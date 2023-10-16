7406ba1c1ddbe60f7334c937dc9e2082cc577300
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-16 22:29:18 +0100

    Go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,36 @@
 #include "main.h"
 
 /**
- *print_rev - prints a string in reverse
- *@s: String to be printed
- *
- */
+*_strlen - returns the length of a string
+*@s: Pointer to a string
+*
+*Return: length of a string
+*/
+
+int _strlen(char *s)
+{
+	int count;
+	for (count = 0; *s != '\0'; s++)
+		count++;
+
+	return (count);
+}
+
+/**
+*print_rev - prints a string in reverse
+*@s: String to be printed
+*
+*/
+
 void print_rev(char *s)
 {
-	int i;
+	int i, rev = 0;
+	int length = _strlen(s);
 
-	while (*s != '\0')
-		s++;
-	for (i = 0; s[i] != s[0]; i--)
-		_putchar(s[i]);
-	_putchar('\n');
+	for (i = 0; i < (length / 2); i++)
+	{
+		rev = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = rev;
+	}
 }
