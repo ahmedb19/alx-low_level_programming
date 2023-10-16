a11176a3ee5f0ca627eef141ae3a2704863771d5
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-16 23:18:31 +0100

    Lies aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *puts2 - prints every other character of a string.
+ *@str: string to be printed
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < str / 2; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
