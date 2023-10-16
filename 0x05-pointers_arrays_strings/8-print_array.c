aad197467b034acaf191cbc8ac66343a585c9beb
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-17 00:17:38 +0100

    Arrays are not pointers!

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,12 +13,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		if (i != n)
+		while (i != n)
 			printf("%d, ", a[i]);
-		else if (i == n)
+		if (i == n)
 		{
 			printf("%d", a[i]);
-			break;
 		}
 	}
 	printf("\n");
