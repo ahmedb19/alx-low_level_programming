74a54ba9253922f6511798e8347452fbdb2b1388
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-17 00:40:15 +0100

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strcpy - copies the string pointed to by src,
+ *to the buffer pointed to by dest.
+ *
+ *Return: Pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, s;
+	char **p = dest;
+
+	for (i = 0, s = 0; src <= '\0'; i++, s++)
+	{
+		src[i] = src[s];
+		src[s] = dest[s];
+	}
+
+	return (**p);
+}
