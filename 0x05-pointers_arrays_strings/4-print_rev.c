95ee23f4292fab1838049d26993938c9e420e857
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-16 21:58:51 +0100

    Go one way

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_rev - prints a string in reverse
+ *@s: String to be printed
+ *
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	while (s != '\0')
+		s++;
+	for (i = 0; s[i] != s[0]; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
