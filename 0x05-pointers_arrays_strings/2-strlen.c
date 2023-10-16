805aea183ca414e0224fccdda1950b9fbc13df40
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-16 19:35:42 +0100

    rrr

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,13 +4,14 @@
 *_strlen - returns the length of a string
 *@s: Pointer to a string
 *
+*Return: length of a string
 */
 int _strlen(char *s)
 {
 	int count;
 
-	for (count = 0; count <= *s, *s != '\0'; count++, *s++)
-		;
+	for (count = 0; s[count] != '\0'; s++)
+		count++;
 
 	return (count);
 }
