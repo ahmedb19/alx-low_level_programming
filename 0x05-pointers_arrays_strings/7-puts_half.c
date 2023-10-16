4214cb465dc6b49b2b22d9baaa08e32aa4a66402
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-17 00:00:59 +0100

    Win while having fun!

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*_strlen - returns the length of a string
+*@s: Pointer to a string
+*
+*Return: length of a string
+*/
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s != '\0'; s++)
+		count++;
+
+	return (count);
+}
+
+/**
+*puts_half - prints half of a string.
+*@str: string to be printed
+*
+*/
+
+void puts_half(char *str)
+{
+	int i, length = _strlen(str);
+
+	for (i = length / 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
