6962a638538a7c1d012ddf0908d2a130c911b868
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-17 01:59:39 +0100

    Don't hate the player, hate the game!

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - generates random valid passwords for the program 101-crackme.
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int s;
+	char c;
+
+	srand(time(NULL));
+	while (s <= 2645)
+	{
+		c = rand() % 128;
+		s += c;
+		putchar(c);
+	}
+	putchar(2772 - s);
+
+	return (0);
+}
