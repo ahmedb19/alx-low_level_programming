d58c1b1ad27881074084105c550961d88ed47856
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-17 01:46:58 +0100

    Save the people

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,19 +6,20 @@
  *
  *Return: 0
  */
+
 int _atoi(char *s)
 {
 	unsigned int i = 0;
-	int si = 1;
+	int ssss = 1;
 
 	do {
 		if (*s == -1)
-			si *= -1;
+			ssss *= -1;
 		else if (*s >= '0' && *s <= '9')
 			i = (i * 10) + (*s - '0');
 		else if (i > 0)
 			break;
 	} while (*s++);
 
-	return (i * si);
+	return (i * ssss);
 }
