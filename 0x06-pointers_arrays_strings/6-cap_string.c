f0d21b785498254d2e706dbe3a0384cbdf759193
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-19 22:49:15 +0100

    Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,12 +8,20 @@
  */
 char *cap_string(char *c)
 {
-	int i;
+	int i, j;
+	char a[] = {' ','\t','\n',',',';','.','!','?','"','(',')','{','}'};
 
 	for (i = 0; c[i] != '\0'; i++)
 	{
 		if (c[i] >= 'a' && c[i] <= 'z')
 			c[i] = c[i] - 32;
+		for (j = 0; j <= 12; j++)
+		{
+			if (a[j] == c[i - 1])
+			{
+				c[i] = c[i] - 32;
+			}
+		}
 	}
 
 	return (c);
