a915e9a8c1eef709da5ea4e2c0ad7ce3cc118ad9
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-19 19:03:29 +0100

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *string_toupper - Changes all lowercase letters of a string to uppercase.
+ *@c: Pointer to a string
+ *
+ *Return: 0
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] = 'A' + i;
+		}
+	}
+	c[i] = '\0';
+
+	return (0);
+}
