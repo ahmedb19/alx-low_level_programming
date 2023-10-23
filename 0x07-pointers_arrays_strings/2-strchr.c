6e9b25f82fa93577da42e11ae638a4a93e2ed5fa
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-23 20:32:27 +0100

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,17 +12,12 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	char *a;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-		{
-			a = &s[i];
-			return (a);
-		}
-		else
-			return (NULL);
+			return (s + i);
 	}
 
+	return (s + i);
 }
