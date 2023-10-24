741877dd5b7f5b94d51652ce908625e4057ae0eb
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-24 01:17:21 +0100

    strpbrk

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,17 +15,20 @@ char *_strstr(char *haystack, char *needle)
 	int i, j;
 	char *s;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (i = 0; haystack[i] > '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		for (j = 0; needle[j] > '\0'; j++)
 		{
-			if (haystack[i] == needle[j])
+			if (haystack[i] != needle[j - i])
 			{
-				s = &haystack[i];
-				return (s);
+				break;
+			}
+			if (needle[j - i] == '\0')
+			{
+				return (haystack + i);
 			}
 		}
 	}
 
-	return ('\0');
+	return (0);
 }
