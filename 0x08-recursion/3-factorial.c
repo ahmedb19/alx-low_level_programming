7d5fec8b1c6a67511f1f4267f130041ae2c479d8
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-25 11:33:37 +0100

    Dream Bigger

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,14 +9,13 @@
 
 int factorial(int n)
 {
-	int i = 0;
-
 	if (n < 0)
-		return (-1);
-	else
 	{
-		i = n * factorial(n - 1);
-		return (i);
+		return (-1);
+	} else if (n == 0)
+	{
+		return(1);
 	}
-	return (0);
+
+	return (n * factorial(n - 1));
 }
