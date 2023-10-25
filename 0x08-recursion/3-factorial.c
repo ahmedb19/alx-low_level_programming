eac7d9cb521e81282eeb808582f6bb654e432159
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-25 11:09:55 +0100

    Dream Bigger

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *factorial - returns the factorial of a given number.
+ *@n: Integer
+ *
+ *Return: factorial of n
+ */
+
+int factorial(int n)
+{
+	int i = 0;
+
+	if (n <= 1)
+		return (1);
+	else
+		i = n * factorial(n - 1);
+
+	return (i);
+}
