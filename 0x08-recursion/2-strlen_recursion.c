ec674e0584a1dac0fbd0030658c79fec7db72b19
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-25 10:57:47 +0100

    Strange Dreams

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - returns the length of a string.
+ *@s: String
+ *
+ *Return: Length of a string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s > '\0')
+	{
+		_strlen_recursion(*s + 1);
+		i++;
+	}
+
+	return (i);
+}
