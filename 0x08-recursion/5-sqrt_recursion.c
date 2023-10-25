5db8ec6578256a1de27da7fd404ac2dc5d6dbeaa
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-25 12:30:01 +0100

    Dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,40 @@
 #include "main.h"
 
 /**
+ *sqr - finds the square root of a number
+ *@val: Integer
+ *@n: Given Value
+ *
+ * Return: Square root
+ */
+
+int _sqr(int val, int n);
+
+/**
  *_sqrt_recursion - returns the natural square root of a number.
  *@n: Given number
  *
  *Return: -1 if n doesn't have a square root
  */
 
-int _sqrt_recursion(int n)
-{
-	int a = 1;
 
-	if (a * a == n)
+
+int _sqr(int val, int n)
+{
+	int val = 1;
+
+	if (val * val == n)
 	{
-		return(a);
-	} else if (a * a < n)
+		return (val);
+	} else if (val * val < n)
 	{
-		return (_sqrt_recursion(n));
+		return (_sqr(val + 1, n));
 	} else
 		return (-1);
+}
+
+int _sqrt_recursion(int n)
+{
+	return (_sqr(1, n));
+
 }
