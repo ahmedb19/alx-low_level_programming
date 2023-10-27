9f35e5dd863c5931404ed5ce4052e6c67b7bd197
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-27 15:15:41 +0100

    What you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - prints its name, followed by a new line.
+ *
+ */
+
+void main(argc, argv[argc])
+{
+	printf("%s", argv[argc]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+# C - argc, argv
+
+In this directory, we will discover command line arguments and how to properly implement while trying to fully grasp the concepts.
