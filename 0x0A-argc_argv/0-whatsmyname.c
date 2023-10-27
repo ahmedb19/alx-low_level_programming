d28f740fb1eacc41d78d2b1a04a7e3451874d5c5
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-27 15:24:22 +0100

    What you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
-
+#define UNUSED(argc) (void)(argc)
 /**
  *main - prints its name, followed by a new line.
+ *@argc: Argument count
+ *@argv: Array of arguments
  *
+ *Return: 0
  */
 
-void main(argc, argv[argc])
+int main(int argc, char *argv[])
 {
-	printf("%s", argv[argc]);
+	UNUSED(argc);
+
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
