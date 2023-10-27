cbbfe5f2f85adde320e7bcfc2001604db8e995d4
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-27 15:47:03 +0100

    Silence

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #define UNUSED(argc) (void)(argc)
+
 /**
  *main - prints its name, followed by a new line.
  *@argc: Argument count
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define UNUSED(argv) (void)(argv)
+
+/**
+ *main - prints the number of arguments passed into it.
+ *@argc: Arguments count
+ *@argv: Array of arguments
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	UNUSED(argv);
+
+	printf("%d\n", argc);
+
+	return (0);
+}
