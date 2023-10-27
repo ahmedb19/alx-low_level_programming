7065c9b92d171096f642f80ea45b2f43cbe949fd
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-27 16:33:00 +0100

    infinity

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - adds positive numbers
+ *@argc: arguments count
+ *@argv: Array of arguments
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	} else if (j <= '0' && j >= '9')
+	{
+		printf("Error\n");
+		return(1);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		j += atoi(argv[i]);
+		printf("%d\n", j);
+	}
+
+	return (0);
+}
