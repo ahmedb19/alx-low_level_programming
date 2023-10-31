1777d6ccaa180ee7f8b3d1aac31b8bd70e9c4a12
Ahmed Bouali <ahmedbouali@proton.me>
2023-10-31 19:27:25 +0100

    No imagination = No wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,13 +19,13 @@ char *_strdup(char *str)
 		;
 
 	a = malloc(s * sizeof(*str) + 1);
+
 	if (a == 0)
 		return (NULL);
-	else
-	{
-		for (i = 0; i < s; i++)
-			a[i] = str[i];
-	}
+
+	for (i = 0; i < s; i++)
+		a[i] = str[i];
+
 
 	return (a);
 }
