2ba23e7567eabcd16bca033582b3ed6be1091904
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-02 19:22:45 +0100

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ *_memset - fills memory with a constant byte
+ *@s: Pointer to a string
+ *@b: constant byte
+ *
+ *Return: Pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
  *_calloc - allocates memory for an array, using malloc.
  *@nmemb: number of elements
  *@size: size of the array
@@ -18,6 +37,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	if (p == 0)
 		return (NULL);
+
+	_memset(p, 0, sizeof(int) * nmemb);
 
 	return (p);
 }
