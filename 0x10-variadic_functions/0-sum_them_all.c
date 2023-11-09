55b4b300c6897e968be0883ed8930d5762dc3648
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-10 00:28:02 +0100

    Beauty is variable

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ *sum_them_all - returns the sum of all its parameters.
+ *@n: Number of parameters
+ *
+ *Return: sum of all its parameters.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int a = 0;
+
+	va_start(args, n);
+
+	for (int i = 0; i < n; i++)
+	{
+		int s = va_arg(args, int);
+		a += s;
+	}
+	va_end(args);
+
+	return (a);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+# C - Variadic functions
+
+In this directory, we will discover variadic functions, how to use them with macros as well as the const keyword.
