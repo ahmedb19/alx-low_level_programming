9f56bcdbc0de6f8132fac8f57ff9f666a0f1f9d4
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-10 02:20:31 +0100

    Constant Woman

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ *print_strings - prints strings
+ *@separator: string to be printed between the strings
+ *@n: number of strings passed to the function
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = n;
+	char *s;
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(args, n);
+	while (i--)
+		printf("%s%s", (s = va_arg(args, char *)) ? s : "(nil)",
+				i ? (separator ? separator : "") : "\n");
+
+	va_end(args;
+}
