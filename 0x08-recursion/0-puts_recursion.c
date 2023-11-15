81049cd7ccaa1450b278df51b3d9b4f7ce8b418a
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-15 20:17:20 +0100

    Secrets

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - prints a string.
+ *@s: String to be printed
+ *
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_puthar(s[i]);
+		i++;
+	}
+	else
+		_puts_recursion(s);
+
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+# C - Recursion
+
+In this directory, we will discover recursion as well as how and when whe should(n't) implement it.
