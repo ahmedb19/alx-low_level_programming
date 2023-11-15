f954bd5d60408263e9c4ff21cbbf993e4c38f44a
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-15 21:27:20 +0100

    Secrets

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,9 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] != '\0')
+	if (s != '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar(s);
+		_puts_recursion(s + 1);
 	}
-	else
-		_puts_recursion(s);
 }
