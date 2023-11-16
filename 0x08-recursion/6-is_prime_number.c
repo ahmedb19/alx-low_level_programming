41ad0e91cd5c68ea893bb3852db95e5e814e54ff
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-16 03:23:13 +0100

    Inception

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,10 +9,10 @@
  */
 int prime_ch(int n, int x)
 {
-	if (n % x == 0 || n <= 1)
-		return (0);
-	else if (x >= n && n > 1)
+	if (x >= n && n > 1)
 		return (1);
+	else if (n % x == 0 || n <= 1)
+		return (0);
 	else
 		return (prime_ch(n, x + 1));
 }
