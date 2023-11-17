addf8943934d31e6c1ef79dc11d593ba0a5682c7
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-17 06:29:58 +0100

    Infinity

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - prints the number of arguments passed into it.
+ *@argc: Number of arguments
+ *@argv: Array of arguments
+ *
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
