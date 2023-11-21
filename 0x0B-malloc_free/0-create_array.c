45b9a3fc0507eeb36a0c3eaf2c64eaabee95623b
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-21 17:38:10 +0100

    Bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,12 +11,10 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
-	unsigned int i;
 
 	ptr = malloc(sizeof(size));
 
-	for (i = 0; i < size; i++)
-		*ptr[i] = c;
+	ptr[i] = c;
 
 	return (ptr);
 }
