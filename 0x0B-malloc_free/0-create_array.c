8d5ed05604dfc827b014f1e8b9c7b4dd35552011
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-21 16:52:49 +0100

    Bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *create_array - creates an array of chars
+ *@size: size of the array
+ *@c: specific char
+ *
+ *Return: NULL if size is 0, else a pointer to the array.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = char ar[malloc(size)];
+
+	a[0] = c;
+
+	return (0);
+}
