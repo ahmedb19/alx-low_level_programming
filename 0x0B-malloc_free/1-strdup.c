9b0d7601102fbbcfc6f78cc5ec825fe2e6bd2f3a
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-21 20:10:47 +0100

    No wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strdup - returns a pointer to a newly allocated space in memory,
+ *which contains a copy of the string given as a parameter.
+ *@str: String to be duplicated.
+ *
+ *Return: NULL if Str is NULL, else a pointer to the duplicated string.
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *s = malloc(sizeof(str));
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		s[i] = str[i];
+
+	return (s);
+}
