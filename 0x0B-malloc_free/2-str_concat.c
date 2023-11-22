5c58d9ee9a87f96c2b1d243f2c8e0a2ec29315d3
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-22 01:23:58 +0100

    Accomplish nothing

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,16 +15,15 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	for (l1 = 0; s1[l1] != '\0'; l1++)
 		;
 	for (l2 = 0; s2[l2] != '\0'; l2++)
 		;
-	ptr = malloc(sizeof(char) * (l1 + l2) + 1);
-
+	ptr = malloc((l1 + l2) * sizeof(char) + 1);
 	if (ptr == 0)
 		return (0);
 
