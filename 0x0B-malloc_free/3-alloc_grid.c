59a08e11a0e62e37d20180def55ba285ebf94caa
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-23 02:15:32 +0100

    Apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,7 +10,26 @@
 
 int **alloc_grid(int width, int height)
 {
+	int s1, s2;
+	char *ptr;
+
 	if (width == 0 || height == 0)
+	{
 		return (NULL);
+	} else
+	{
+		ptr = malloc(sizeof(char) * (height + width));
+		if (ptr == 0)
+			return (0);
+		for (s1 = 0; s1 < width; s1++)
+		{
+			ptr[s1][0] = {0, 0};
+			for (s2 = 0; s2 < height; s2++)
+			{
+				ptr[0][s2] = {0, 0};
+			}
+		}
+	}
+	return (ptr);
 
 }
