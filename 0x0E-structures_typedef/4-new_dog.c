b0675c138b054ccf7cfd13f20eda672826ec5665
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-28 00:56:19 +0100

    Wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,23 +1,80 @@
 #include "dog.h"
 
 /**
- *new_dog - creates a new dog
- *@name: name of the new dog
- *@age: age of the new dog
- *@owner: owner of the new dog
- *
- *Return: NULL if the function fails, otherwise 0
- */
+*_strlen - returns the length of a string
+*@s: Pointer to a string
+*
+*Return: length of a string
+*/
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s != '\0'; s++)
+		count++;
+
+	return (count);
+}
+
+/**
+*_strcpy - copies the string pointed to by src,
+*to the buffer pointed to by dest.
+*@dest: pointer to buffer
+*@src: pointer to string
+*
+*Return: Pointer to dest.
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+
+/**
+*new_dog - creates a new dog
+*@name: name of the new dog
+*@age: age of the new dog
+*@owner: owner of the new dog
+*
+*Return: NULL if the function fails, otherwise 0
+*/
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (new_dog)
-	{
-		new_dog->name = name;
-		new_dog->age = age;
-		new_dog->owner = owner;
-	}
-	else
+	dog_t dog;
+
+	if (!name || age < 0 || !owner)
 		return (NULL);
-	return (0);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	dog->name = _strcpy(dog->name, name);
+	dog->age = age;
+	dog->owner = _strcpy(dog->owner, owner);
+
+	return (dog);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - struct contains dog's info
  *@name: dog's name
@@ -9,13 +12,14 @@
  *
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
