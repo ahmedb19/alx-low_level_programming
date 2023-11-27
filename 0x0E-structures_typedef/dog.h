051d5338687744b6a391734c705075667abeea0e
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-27 22:33:00 +0100

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef _DOG_H
 #define _DOG_H
 
-struct dog {
+/**
+ * struct dog - struct contains dog's info
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: Owner of the dog
+ *
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
