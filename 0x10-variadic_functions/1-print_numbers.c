55dc3ace6749f7651df83818731b686e72439b0d
Ahmed Bouali <ahmedbouali@proton.me>
2023-11-30 22:43:37 +0100

    Value of a variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,14 +14,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	int s;
 
 	va_start(args, n);
+
 	for (i = 0; i <= n; i++)
 	{
 		s = va_arg(args, int);
-		if (separator != NULL && i < n)
+		if (n == i)
 		{
+			printf("\n");
+			break;
+		}
+		if (separator != NULL && i < n)
 			printf("%d%s", s, separator);
-		} else if (i == n)
-			printf("%d\n", s);
 	}
 
 	va_end(args);
