b395d06ffcdeb9bd1c0c8f8dfe2ab0b977192586
Ahmed Bouali <ahmedbouali@proton.me>
2024-01-11 00:30:02 +0100

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - prints a string in reverse.
+ *@s: Given string
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	while (i > 0)
+	{
+		_putchar(s[i]);
+		_print_rev_recursion(s[i - 1]);
+	}
+}
