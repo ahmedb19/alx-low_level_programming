0ec3d2d8791abcf52e5d946ae4a2ee8f76329300
Ahmed Bouali <ahmedbouali@proton.me>
2024-01-11 01:21:16 +0100

    1. We are together

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,13 +8,13 @@
 
 void _print_rev_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	while (i > 0)
+	if (*s != '\0')
 	{
-		_putchar(s[i]);
-		_print_rev_recursion(*s[i - 1]);
+		_print_rev_recursion(s + 1);
+		if (*s == '\0')
+		{
+			_putchar(s);
+			_print_rev_recursion(s - 1);
+		}
 	}
 }
