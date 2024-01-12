e6bb943268c1c2d10a27b69103f797b33ca681b8
Ahmed Bouali <ahmedbouali@proton.me>
2024-01-12 23:12:04 +0100

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,9 +12,20 @@
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
-
+	
 	for (i = 0; i < argc; i++)
+	{
 		sum += atoi(argv[i]);
+
+		if (argc < 0)
+			printf("0\n");
+		else if (atoi(argv[i]) >= 'a' && atoi(argv[i] <= 'z'))
+		{
+			printf("Error\n");
+			return (0);
+		}
+
+	}
 	printf("%d\n", sum);
 
 	return (0);
