095a1c04e649d3f3f50711e3a6f7a2a518dd3d79
Ahmed Bouali <ahmedbouali@proton.me>
2024-01-13 22:04:22 +0100

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,18 @@
  */
 int main(int argc, char *argv[])
 {
+	int a, b;
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
 	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
-	{
-		int a, b;
-
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
 		printf("%d\n", a * b);
-	}
+
 	return (0);
 }
