f81501081ab1f1d3cddaf8a2c457a12ef6f3dfaf
Ahmed Bouali <ahmedbouali@proton.me>
2024-01-16 22:35:21 +0100

    2. Accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *str_concat - concatenates two strings
+ *@s1: 1st string
+ *@s2: 2nd string
+ *
+ *Return: Pointer on success Otherwise NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int l1, l2, s, i;
+
+	for (l1 = 0; s1[l1] != '\0'; l1++)
+		;
+	for (l2 = 0; s2[l2] != '\0'; l2++)
+		;
+	s = l1 + l2 + 1;
+
+	char *a = malloc(sizeof(char) * size);
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		if (a)
+		{
+			for (i = 0; i < l1; i++)
+				a[i] = s1[1];
+			for (i = 0; i < l2; i++)
+				a[l1 + i] = s2[i];
+			s[size - 1] = '\0';
+		}
+		else
+			return (NULL);
+	}
+	return (a);
+}
