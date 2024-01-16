f2826f37ec09fcb63dec62f130789c5d2cf6108f
Ahmed Bouali <ahmedbouali@proton.me>
2024-01-16 09:19:20 +0100

    0. Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *create_array - creates an array of chars, and initializes it
+ *with a specific char.
+ *@size: Size of the array
+ *@c: Specific char
+ *
+ *Return: Pointer to the memory location.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+	else
+	{
+		int *a = malloc(size);
+		int i;
+
+		for (i = 0; i < size; i++)
+			a[i] = c;
+		return (a);
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+# C - malloc, free
+
+In this directory, we will handle dynamic memory allocation while solving some interesting challenges as well as discovering 2 new functions(_malloc*,* free_).
