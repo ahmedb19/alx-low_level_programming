6340ec6213ff357770ca3e9fbd9b099d36529881
Ahmed Bouali <ahmedbouali@proton.me>
2024-01-16 09:33:04 +0100

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,23 +12,18 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *a;
+	char *a = malloc(size);
 
-	if (size == 0)
+	for (i = 0; i < size; i++)
 	{
-		return ('\0');
-	}
-	else
-	{
-		a = malloc(size);
-
-		if (*a != 0)
+		if (size == 0)
 		{
-			for (i = 0; i < size; i++)
-				a[i] = c;
-			return (a);
+			return ('\0');
 		}
 		else
-			return ('\0');
+		{
+			a[i] = c;
+		}
 	}
+	return (a);
 }
