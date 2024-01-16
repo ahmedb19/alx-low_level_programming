9fce95549d15bf851cd0b443e7e16579c0f93fb9
Ahmed Bouali <ahmedbouali@proton.me>
2024-01-16 22:49:48 +0100

    2. Accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,19 +13,19 @@ char *str_concat(char *s1, char *s2)
 	int l1, l2, s, i;
 	char *a;
 
+	if (s1 == NULL)
+		s1 = NULL;
+	if (s2 == NULL)
+		s2 = NULL;
+
 	for (l1 = 0; s1[l1] != '\0'; l1++)
 		;
 	for (l2 = 0; s2[l2] != '\0'; l2++)
 		;
 	s = l1 + l2 + 1;
 	a = malloc(sizeof(char) * s);
-
 	if (a == 0)
 		return (0);
-	if (s1 == NULL)
-		s1 = '\0';
-	if (s2 == NULL)
-		s2 = '\0';
 	if (a)
 	{
 		for (i = 0; i < l1; i++)
