1f2a34edf4a0a201b994b2a34372d7adc396090c
Ahmed Bouali <ahmedbouali@proton.me>
2024-01-19 02:22:40 +0100

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -28,16 +28,16 @@ char *_memset(char *s, char b, unsigned int n)
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *s;
+	void *a;
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	s = malloc(sizeof(int) * nmemb);
+	a = malloc(sizeof(int) * nmemb);
 
-	if (s == 0)
+	if (a == 0)
 		return (NULL);
 
-	_memset(s, 0, sizeof(int) * nmemb);
+	_memset(a, 0, sizeof(int) * nmemb);
 
-	return (s);
+	return (a);
 }
