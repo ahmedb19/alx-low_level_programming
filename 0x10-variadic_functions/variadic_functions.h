e10252b02e9dd2cd2834cbe659d4e59163bdbbcf
Ahmed Bouali <ahmedbouali@proton.me>
2024-01-26 23:32:24 +0100

    variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,19 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ *struct token - Struct token
+ *
+ *@token: format token
+ *@f: function associated
+ *
+ */
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+} token_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
