3fdc88415be6e3c7cde007667cc1c1a4d32a046a
Ahmed Bouali <ahmedbouali@proton.me>
2024-01-28 23:41:35 +0100

    Update

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ *main - prints the minimum number of coins
+ * to make change for an amount of money.
+ *@argc: Arguments count
+ *@argv: Array of Args
+ *
+ *Return: 
+ */
