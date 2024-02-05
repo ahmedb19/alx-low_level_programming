b8e1cbca371feb43401cbce277b2c8112c1e562d
Ahmed Bouali <ahmedbouali@proton.me>
2024-02-05 21:51:47 +0100

    Print List

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ *_strlen - returns the length of a string
+ *@s: Pointer to a string
+ *
+ *Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s != '\0'; s++)
+		count++;
+
+	return (count);
+}
+
+/**
+ *print_list - prints all the elements of list_t
+ *@h: list
+ *
+ *Return: Number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		i++;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# C - Singly linked lists
+
+In this directory, we will discover data structures and linked lists, how to create, add or remove elements within them as well as when and why we should use linked lists instead of arrays.
