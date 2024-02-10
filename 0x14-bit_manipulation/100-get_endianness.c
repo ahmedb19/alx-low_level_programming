5e43230afbec957417057e46b373cab64dc856d9
Ahmed Bouali <ahmedbouali@proton.me>
2024-02-10 03:47:24 +0100

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *get_endiannes - returns the endiannes of the system
+ *
+ *Return: 0 if endian is big, otherwise 1
+ */
+
+int get_endianness(void)
+{
+	unsigned long int n = 1;
+
+	return (*(char *)&n);
+}
